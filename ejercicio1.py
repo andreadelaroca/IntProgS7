b04c61f5b7e1f21f56847eda009bcffc69ca3def
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-07 13:12:31 -0600

    Ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,12 @@
 #suma de los primeros n numeros
+
+while True:
+    n = int(input("Ingrese un número entero positivo: "))
+    if n > 0:
+        break
+    print("Intente otra vez")
+
+for i in range(1,n+1):
+    resultado = n + i
+    
+    print(f"{n} + {i} = {resultado}")
