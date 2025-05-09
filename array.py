03922c6525820db090a32b1b85c759ce0cb2b51d
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-09 16:32:19 -0600

    Agregué comentarios al ejercicio

--- array.py
+++ array.py
@@ -1,29 +1,18 @@
+#array: estructura de datos que almacena elementos del mismo tipo
+
 numero = input("Dime un número: ")
 
-numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-repetidos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+repetidos = [0] * 10
+#repetidos crea una lista de 10 veces 0 como índices
 
 for digito in numero:
-    elif digito == "2:"
-    elif digito == "3:"
-        repetidos[3] += 1
-    elif digito == "1":
-        repetidos[2] += 2
-    elif digito == "2":
-        repetidos[3] += 3
-    elif digito == "3":
-        repetidos[4] += 4
-    elif digito == "4":
-        repetidos[5] += 5
-    elif digito == "5":
-        repetidos[6] += 6
-    elif digito == "6":
-        repetidos[7] += 7
-    elif digito == "7":
-        repetidos[8] += 8
-    elif digito == "8":
-        repetidos[9] += 9
-    elif digito == "9":
-        repetidos[1] += 1
-    
-for i, j in 
+    if digito.isdigit():
+        repetidos[int(digito)] += 1
+#isdigit asegura que sea un digito
+
+for i in range(10):
+    if repetidos[i] > 0:
+        print(f"El digito {i} aparece {repetidos[i]} veces.")
+#se convierte el dígito a número y se suma uno
+# ej: 2, el digito 2 aparece 1 veces
+#fua chaval
