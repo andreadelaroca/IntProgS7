9a224d6be425e92e9f487cf2788eb141263faf75
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-09 16:22:25 -0600

    Array

---
+++ array.py
@@ -0,0 +1,29 @@
+numero = input("Dime un número: ")
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+repetidos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for digito in numero:
+    elif digito == "2:"
+    elif digito == "3:"
+        repetidos[3] += 1
+    elif digito == "1":
+        repetidos[2] += 2
+    elif digito == "2":
+        repetidos[3] += 3
+    elif digito == "3":
+        repetidos[4] += 4
+    elif digito == "4":
+        repetidos[5] += 5
+    elif digito == "5":
+        repetidos[6] += 6
+    elif digito == "6":
+        repetidos[7] += 7
+    elif digito == "7":
+        repetidos[8] += 8
+    elif digito == "8":
+        repetidos[9] += 9
+    elif digito == "9":
+        repetidos[1] += 1
+    
+for i, j in 
