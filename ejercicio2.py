0de531d9caef6dcc79727290292ab6447cf8b1bd
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-09 17:00:57 -0600

    Ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,17 @@
 #producto de los primeros m números pares
 #usar cont, while y acumulador
+#acumulador para el producto
+#contador para los pares
 
-cont = 1
 m = int(input("Ingrese un número entero positivo: "))
+
+contador = 0
+par = 2
+producto = 1
+
+while contador < m:
+    producto *= par
+    par += 2
+    contador += 1
+    
+print(f"El producto de los primeros {m} números pares es: {producto}")
