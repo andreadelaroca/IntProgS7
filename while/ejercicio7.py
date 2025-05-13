6e83369b2c0c38765e2dcc8154a70f2bdfc3bcd0
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-13 13:24:32 -0600

    Añadí la función while y la expliqué con comentarios

--- while/ejercicio7.py
+++ while/ejercicio7.py
@@ -1,22 +1,20 @@
 #suma de números pares e impares
-#Calcula la suma de los números pares y la suma de los números impares
 #usar while, acumladores para la suma de pares e impares
 
-numeros = input("Ingresa una lista de números separados por espacios (utilice 0 como valor final): ")
+numeros = input("Ingresa una lista de números separados por espacios: ")
 
 numeros = numeros.split()
 suma_pares = 0
 suma_impares = 0
 cont = 0
 
-while cont < len(numeros): #len es la longitud de la lista
-    if int(numeros[cont]) == 0:
-        break
-    if int(numeros[cont]) % 2 == 0:
-        suma_pares += int(numeros[cont])
+while cont < len(numeros): #mientras cont sea menor que la longitud de la lista
+    numero = int(numeros[cont]) #convertir el número a entero
+    if numero % 2 == 0:
+        suma_pares += numero
     else:
-        suma_impares += int(numeros[cont])
+        suma_impares += numero
     cont += 1
-        
+    
 print(f"La suma de los números pares es: {suma_pares}")
 print(f"La suma de los números impares es: {suma_impares}")
